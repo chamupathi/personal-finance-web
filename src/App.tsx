b266294a3fd0c28a64e15e0a368b5ce2e57d0e12
Chamupathi Mendis <chamupathi.mendis@thirdfort.com>
2020-07-12 14:21:42 +0530

    record created hash routere added

--- src/App.tsx
+++ src/App.tsx
@@ -2,24 +2,43 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+import { HashRouter as Router, Route, Link, Switch } from 'react-router-dom'
+import Record from './records/record';
+
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    // <div className="App">
+    //   <header className="App-header">
+    //     <img src={logo} className="App-logo" alt="logo" />
+    //     <p>
+    //       Edit <code>src/App.tsx</code> and save to reload.
+    //     </p>
+    //     <a
+    //       className="App-link"
+    //       href="https://reactjs.org"
+    //       target="_blank"
+    //       rel="noopener noreferrer"
+    //     >
+    //       Learn React..
+    //     </a>
+    //   </header>
+    // </div>
+    <Router>
+        <div className="App">
+          <nav>
+            <Link to="/">Home</Link>
+            <Link to="/record">  Record  </Link>
+            <Link to="/bar">Bar</Link>
+          </nav>
+          <Switch>
+            {/* <Route exact path="/" component={Home} /> */}
+            <Route exact path="/" component={Record} />
+            <Route exact path="/record" component={Record} />
+            {/* <Route exact path="/bar" component={Bar} /> */}
+          </Switch>
+          
+        </div>
+      </Router>
   );
 }
 
---
+++ src/records/record.tsx
@@ -0,0 +1,10 @@
+import * as React from 'react';
+
+interface IRecordProps {
+}
+
+const Record: React.FunctionComponent<IRecordProps> = () => {
+  return <div>Record</div>;
+};
+
+export default Record;
